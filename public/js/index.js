9773301e23ec81b1c5dabd2a046c50744fa222cb
Bohdan Basov <basovbohdan@gmail.com>
2017-05-31 14:46:15 +0000

    Add greating to the chat

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,16 @@
 var socket = io();
         
-socket.on('connect', function() {
+socket.on('connect', function(msg) {
     console.log('Connected!');
 });
 
 socket.on('disconnect', function() {
     console.log('Disconnected!');    
 });
+
+socket.on('new user', function(message){
+    console.log(message);
+})
 
 socket.on('newMessage', function(message) {
     console.log(message);
--- server/server.js
+++ server/server.js
@@ -13,12 +13,24 @@ const io = socket(server);
 io.on('connection', (socket) => {
     console.log('New user connected');
     
+    socket.emit('new user', {
+        from: 'Admin',
+        text: "Welcome to the chat app",
+        createdAt: new Date().getTime()
+    });
+    
+    socket.broadcast.emit('new user', {
+        from: "Admin",
+        text: 'New user joined us',
+        createdAt: new Date().getTime()
+    });
+    
     socket.on('createMessage', (message) => {
         console.log(message);
         io.emit('newMessage', {
             from: message.from,
             text: message.text,
-            createdAt: new Date
+            createdAt: new Date().getTime()
         });
     });
     
